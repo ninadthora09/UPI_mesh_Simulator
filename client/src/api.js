3d5631f457983fc3dafd0c8951b804dbe8e1518e
Ninad Thorat <programmingwithninadthorat4@gmail.com>
2026-05-27 01:04:28 +0530

    Updated api call

--- client/src/api.js
+++ client/src/api.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
 
-export const getAccounts     = ()     => axios.get('/api/accounts')
-export const getTransactions = ()     => axios.get('/api/transactions')
-export const getMeshState    = ()     => axios.get('/api/mesh/state')
-export const sendPayment     = (data) => axios.post('/api/demo/send', data)
-export const runGossip       = ()     => axios.post('/api/mesh/gossip')
-export const flushBridge     = ()     => axios.post('/api/mesh/flush')
-export const resetMesh       = ()     => axios.post('/api/mesh/reset')
+const API = import.meta.env.VITE_API_URL
+
+export const getAccounts     = ()     => axios.get(`${API}/api/accounts`)
+export const getTransactions = ()     => axios.get(`${API}/api/transactions`)
+export const getMeshState    = ()     => axios.get(`${API}/api/mesh/state`)
+export const sendPayment     = (data) => axios.post(`${API}/api/demo/send`, data)
+export const runGossip       = ()     => axios.post(`${API}/api/mesh/gossip`)
+export const flushBridge     = ()     => axios.post(`${API}/api/mesh/flush`)
+export const resetMesh       = ()     => axios.post(`${API}/api/mesh/reset`)
